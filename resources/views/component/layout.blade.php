8a7e0d67a0120ec57d0b7a6e35f08ea777137ae9
na0260 <nur.201002398@green.ac.bd>
2024-02-08 18:54:19 +0600

    layout ui added

---
+++ resources/views/component/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{$tittle ?? "CRUD"}}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+    @yield('content')
+</div>
+
+</body>
+</html>
