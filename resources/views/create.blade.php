335477a8cc20882f94f83d2572d49e895ff13b66
na0260 <nur.201002398@green.ac.bd>
2024-02-08 19:32:10 +0600

    student create page ui added and rendered

---
+++ resources/views/create.blade.php
@@ -0,0 +1,16 @@
+<x-app tittle="create">
+    <x-slot name="content">
+        <h2 class="text-center">Create New Student</h2>
+        <form action="">
+            <div class="mb-3 mt-3">
+                <label for="FirstName" class="form-label">First Name:</label>
+                <input type="text" class="form-control" id="FirstName" placeholder="Enter Your First Name" name="FirstName">
+            </div>
+            <div class="mb-3">
+                <label for="LastName" class="form-label">Last Name:</label>
+                <input type="text" class="form-control" id="LastName" placeholder="Enter Your Last Name" name="LastName">
+            </div>
+            <input type="submit" value="Add" class="btn btn-primary">
+        </form>
+    </x-slot>
+</x-app>
