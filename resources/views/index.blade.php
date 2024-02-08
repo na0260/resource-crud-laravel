5966b306ab554da408839facf05524311071dcdf
na0260 <nur.201002398@green.ac.bd>
2024-02-08 18:58:58 +0600

    Home ui added

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<x-layout tittle="Home">
+    <x-slot name="content">
+        <h2>Student Table</h2>
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+            </tr>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+            </tr>
+            <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+            </tr>
+            </tbody>
+        </table>
+    </x-slot>
+</x-layout>
